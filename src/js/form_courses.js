906b2999d0a4bd90ea4436f4ba5c7b4a21f50118
Jonathan Caixba <caixbajonathanfm@gmail.com>
2026-01-21 10:39:47 -0600

    add: form_courses.js y courses.json en carpeta data

---
+++ src/js/form_courses.js
@@ -0,0 +1,33 @@
+const container = document.getElementById("courses-container");
+
+fetch("../data/courses.json")
+  .then(res => res.json())
+  .then(courses => {
+    courses.forEach(course => {
+      const article = document.createElement("article");
+      article.classList.add("course");
+
+      article.innerHTML = `
+        <img src="${course.image}" class="courses-img" />
+        <header>
+          <h1 class="course-header">${course.title}</h1>
+          <p class="course-description">${course.description}</p>
+        </header>
+        <div class="course-financing">
+          <p class="course-price">${course.price}</p>
+          <div class="course-reviews">
+            <span>${course.rating}</span>
+            <span>${course.level}</span>
+            <span>${course.tag}</span>
+          </div>
+          <div class="buttons">
+            <button class="btn-courses buy">Comprar</button>
+            <button class="btn-courses outline">Ver curso</button>
+          </div>
+        </div>
+      `;
+
+      container.appendChild(article);
+    });
+  })
+  .catch(err => console.error("Error cargando cursos", err));
